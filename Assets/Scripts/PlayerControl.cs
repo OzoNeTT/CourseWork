355505596b1202ed8adf7b976d017affed7d066b
Alexey Gorbachev <Alexey1020@gmail.com>
2019-10-14 19:04:47 +0300

    ddd

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float moveSpeed;
+   // public bool canDoubleJump;
+    public KeyCode R;
+    public KeyCode L;
+   // public KeyCode spacebar;
+   // public float jumpheight;
+    public bool isFacingRight;
+   // public Transform groundCheck;
+   // public float groundCheckRadius;
+    //public LayerMask whatIsGround;
+   // private bool grounded;
+    //public int collectedCoins = 0;
+    float fx, fy, fz;
+    public float KnockBackDist;
+    public float KnockBackCount;
+    public bool KnockFromRight;
+    private float moveVelocity;
+    public KeyCode Return;
+    //public Transform firePoint;
+   // public GameObject Bullet;
+   public double nextFire = -1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextFire = Time.time;
+        fx = transform.localScale.x;
+        fy = transform.localScale.y;
+        fz = transform.localScale.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVelocity = 0;
+        if (Input.GetKey(R))
+            moveVelocity = moveSpeed;
+        else if (Input.GetKey(L))
+            moveVelocity = -moveSpeed;
+
+        if (KnockBackCount <= 0)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+            flip();
+        }
+        else
+        {
+            if (KnockFromRight)
+                GetComponent<Rigidbody2D>().velocity = new Vector2(-KnockBackDist, GetComponent<Rigidbody2D>().velocity.y);
+            if (!KnockFromRight)
+                GetComponent<Rigidbody2D>().velocity = new Vector2(KnockBackDist, GetComponent<Rigidbody2D>().velocity.y);
+            KnockBackCount -= Time.deltaTime;
+        }
+
+        GetComponent<Animator>().SetFloat("Velocity", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+    }
+   
+
+     void flip()
+    {
+        if (GetComponent<Rigidbody2D>().velocity.x > 0)
+        {
+            transform.localScale = new Vector3(fx, fy, fz);
+
+        }
+        else if (GetComponent<Rigidbody2D>().velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1 * fx, fy, fz);
+
+        }
+    }
+    
+}
---
+++ Assets/Scripts/SpikesController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikesController : MonoBehaviour {
+
+    public int damage = 6;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            //FindObjectOfType<LevelManger>().Respawnplayer();
+            //FindObjectOfType<PlaterStats>().TakeDamage(damage);
+            other.GetComponent<PlayerControl>().KnockBackCount = 0.2f;
+            if (other.transform.position.x < this.transform.position.x)
+               other.GetComponent<PlayerControl>().KnockFromRight = true;
+        
+        }
+    }
+}
